01249ebe887e69126893d58570572b644a806cd8
Sheldon10095 <18726387+Sheldon10095@users.noreply.github.com>
2022-05-08 18:53:20 -0400

    Create ILayoutEditor.cs

---
+++ Plugins/SampleMapEditor/Editors/MapEditor/ILayoutEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GLFrameworkEngine;
+using MapStudio.UI;
+using Toolbox.Core.ViewModels;
+
+namespace SampleMapEditor.LayoutEditor
+{
+    public interface ILayoutEditor
+    {
+        string Name { get; }
+
+        StageLayoutPlugin MapEditor { get; set; }
+
+        IToolWindowDrawer ToolWindowDrawer { get; set; }
+
+        List<IDrawable> Renderers { get; set; }
+        NodeBase Root { get; set; }
+        List<MenuItemModel> MenuItems { get; set; }
+
+        List<NodeBase> GetSelected();
+
+        bool IsActive { get; set; }
+
+        void ReloadEditor();
+
+        void DrawHelpWindow();
+        void DrawEditMenuBar();
+
+        void RemoveSelected();
+
+        void OnMouseMove(MouseEventInfo mouseInfo) { }
+        void OnMouseDown(MouseEventInfo mouseInfo);
+        void OnMouseUp(MouseEventInfo mouseInfo);
+        void OnKeyDown(KeyEventInfo keyInfo);
+        void OnSave(StageDefinition stage);
+    }
+}
